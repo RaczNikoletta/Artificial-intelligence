229d869e6b719419d0ce19d8278011b8a5bc8426
Rácz Nikoletta <44648333+RaczNikoletta@users.noreply.github.com>
2026-02-09 19:21:12 +0100

    Correct weight initialization and add PyTorch layer
    Fix initialization of weights and bias in TensorFlow layer and add a PyTorch equivalent implementation.

--- MIT_6.S191 /lesson1.py
+++ MIT_6.S191 /lesson1.py
@@ -3,18 +3,37 @@ class MyDenseLayer (tf.keras.layers.Layer):
   def __init__(self, input_dim, output_dim):
     super(MyDenseLayer, self) __init__()
 
-#Initialize weights and bias
+    #Initialize weights and bias
 
-self.W = self.add_weight([input_dime, output_dim])
-self.b = self.add_weight([1, output_dime])
+    self.W = self.add_weight([input_dim, output_dim])
+    self.b = self.add_weight([1, output_dim])
 
 def call(self, inputs):
   #forward propagate the input
   z = tf.matmul(inputs, self.W) + self.b
 
-#Feed through
-output = tf.math.sigmoid(z)
-return output
+  #Feed through
+  output = tf.math.sigmoid(z)
+  return output
+
+#Pytorch
+class MyDenseLayer(nn.Module):
+  def __init__(self,input_dim, output_dim):
+    super(MyDenseLayer, self).__init__()
+    #Initialize weights and bias
+    self.W = nn.Parameter(torch.randn(input_dim, 
+                                      output_dim, requires_grad=True)
+    self.b = nn.Parameter(torch.randn(1, output_dim, requires_grad=True)
+
+    def forward(self, inputs):
+      #Forward propagate the inputs
+      z = torch.matmul(inputs, self.W) + self.b
+      # Feed through a non-linear activation
+      output = torch.sigmoid(z)
+      return output
+
+
+
 
 
   
