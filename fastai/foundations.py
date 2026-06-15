2da859b6cba539398c78ccfdfb296f81f7e302c3
Rácz Nikoletta <44648333+RaczNikoletta@users.noreply.github.com>
2026-06-15 14:21:40 +0200

    Implement matrix multiplication function in foundations.py

---
+++ fastai/foundations.py
@@ -0,0 +1,15 @@
+import torch
+from torch import tensor
+
+def matmul(a,b):
+  ar,ac = a.shape # n_rows * n_cols
+  br,bc = b.shape
+  assert ac == br
+  c = torch.zeros(ar, bc)
+  for i in range(ar):
+    for j in range(bc):
+      for k in range(ac): c[i,j] += a[i,k] * b[k,j]
+  return c
+
+m1 = torch.randn(5,28*28)
+m2 = torch.randn(784,10)
